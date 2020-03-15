35322360e1430b70b2ba2f2c26b4687c21afcbfb
Amber Williams <awilliams1234@live.com>
2020-03-15 21:53:06 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,15 +1,19 @@
 class Triangle
   
   def initialize(first_side, second_side, third_side)
-    first_side = []
-    second_side = []
-    third_side = []
+    @first_side = first_side
+    @second_side = second_side
+    @third_side = third_side
   end 
+  
   
   def equilateral
-    first_side = second_side = third_side
+    first_side == second_side == third_side
   end 
   
-  
-  
+  def isosceles
+  first_side = second_side && second_side != third_side
+end 
+
+
 end
