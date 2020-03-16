e25986f0de230d1e01963bc2bd0b3a890d8204dd
Terry Threatt <terry.threatt@gmail.com>
2020-03-16 01:40:59 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
-end
+  attr_accessor :angle_1, :angle_2, :angle_3
+  
+    def initialize (angle_1, angle_2, angle_3)
+        @angle_1 = angle_1
+        @angle_2 = angle_2
+        @angle_3 = angle_3
+    end
+    
+    def kind
+        if angle_1 <= 0 || angle_2 <= 0 || angle_3 <= 0
+            begin
+                raise TriangleError
+            end
+        elsif angle_1 == nil || angle_2 == nil || angle_3 == nil
+            begin
+                raise TriangleError
+            end
+        elsif angle_1 + angle_2 <= angle_3 || angle_1 + angle_3 <= angle_2 || angle_3 + angle_2 <= angle_1
+            begin
+                raise TriangleError
+            end
+        else
+          if angle_1 == angle_2 && angle_1 == angle_3
+              :equilateral
+          elsif angle_1 == angle_2 || angle_1 == angle_3 || angle_2 == angle_3
+              :isosceles
+          else
+              :scalene
+          end
+        end 
+    end
+
+  class TriangleError < StandardError
+    puts "There's an Triangle Error"
+  end
+  
+end 
