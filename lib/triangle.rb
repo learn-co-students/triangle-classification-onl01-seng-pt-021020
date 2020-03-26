f4b9b2ce3fbe403cdfcb6061ddddfb0ee1a04bcd
Amber Williams <awilliams1234@live.com>
2020-03-26 18:23:48 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,32 +1,33 @@
 class Triangle
-  
+
   def initialize(first_side, second_side, third_side)
     @first_side = first_side
     @second_side = second_side
     @third_side = third_side
     @all_sides = [first_side, second_side, third_side]
-  end 
-  
+  end
+
   def triangle_valid?
-    if @first_side > 0 && @second_side > 0 && @third_side > 0 
-      return true 
+    if @first_side > 0 && @second_side > 0 && @third_side > 0
+      return true
     else
-      TriangleError
-    end 
-  end 
-  
-  def kind 
-    if @first_side == @second_side && @second_side == @third_side
-      :equilateral
-    elsif @first_side == @second_side && @second_side != @third_side && @side_three != @first_side|| @second_side == @third_side && @third_side != @first_side || @first_side == @third_side && @first_side != @second_side
-      :isosceles
-    elsif @first_side != @second_side && @second_side != @third_side
-      :scalene
-    else @all_sides != triangle_valid?
-      raise TriangleError
-    end 
-  end 
-  
+      puts "illegal"
+    end
+  end
+
+  def kind
+    if valid?
+      if @first_side == @second_side && @second_side == @third_side
+        :equilateral
+      elsif @first_side == @second_side && @second_side != @third_side && @side_three != @first_side|| @second_side == @third_side && @third_side != @first_side || @first_side == @third_side && @first_side != @second_side
+        :isosceles
+      elsif @first_side != @second_side && @second_side != @third_side
+        :scalene
+      else
+        raise TriangleError
+      end
+    end
+    
   class TriangleError < StandardError
-  end 
-end 
+  end
+end
