a8f3b8268cebd69f20096185f3e16ce2d3a48a11
Amber Williams <awilliams1234@live.com>
2020-03-26 18:34:23 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,27 +7,26 @@ class Triangle
     @all_sides = [first_side, second_side, third_side]
   end
 
-  def triangle_valid?
-    if @first_side > 0 && @second_side > 0 && @third_side > 0
-      return true
-    else
-      puts "illegal"
-    end
-  end
+  # def triangle_valid?
+  #   if @first_side > 0 && @second_side > 0 && @third_side > 0
+  #     return true
+  #   else
+  #     puts "illegal"
+  #   end
+  # end
 
   def kind
-    if valid?
-      if @first_side == @second_side && @second_side == @third_side
-        :equilateral
-      elsif @first_side == @second_side && @second_side != @third_side && @side_three != @first_side|| @second_side == @third_side && @third_side != @first_side || @first_side == @third_side && @first_side != @second_side
-        :isosceles
-      elsif @first_side != @second_side && @second_side != @third_side
-        :scalene
-      else
-        raise TriangleError
-      end
+    if @first_side == @second_side && @second_side == @third_side
+      :equilateral
+    elsif @first_side == @second_side && @second_side != @third_side && @side_three != @first_side|| @second_side == @third_side && @third_side != @first_side || @first_side == @third_side && @first_side != @second_side
+      :isosceles
+    else @first_side != @second_side && @second_side != @third_side
+      :scalene
     end
-    
-  class TriangleError < StandardError
   end
 end
+
+
+#   class TriangleError < StandardError
+#   end
+# end
