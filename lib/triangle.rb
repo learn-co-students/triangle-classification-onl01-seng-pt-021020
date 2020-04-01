81d01ffb9542b555a715dbaa48a2e1c038705b71
Amber Williams <awilliams1234@live.com>
2020-04-01 19:13:06 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,32 +1,26 @@
 class Triangle
 
+
   def initialize(first_side, second_side, third_side)
     @first_side = first_side
     @second_side = second_side
     @third_side = third_side
-    @all_sides = [first_side, second_side, third_side]
   end
-
-  # def triangle_valid?
-  #   if @first_side > 0 && @second_side > 0 && @third_side > 0
-  #     return true
-  #   else
-  #     puts "illegal"
-  #   end
-  # end
 
   def kind
     if @first_side == @second_side && @second_side == @third_side
       :equilateral
     elsif @first_side == @second_side && @second_side != @third_side && @side_three != @first_side|| @second_side == @third_side && @third_side != @first_side || @first_side == @third_side && @first_side != @second_side
       :isosceles
-    else @first_side != @second_side && @second_side != @third_side
+    elsif @first_side != @second_side && @second_side != @third_side
       :scalene
+    elsif (@first_side <= 0) || (@second_side <= 0) || (@third_side <= 0)
+      raise TriangleError
+    else (@first_side + @second_side <= @third_side) || (@first_side + @third_side <= @second_side) || (@second_side + @third_side <= @first_side)
+      raise TriangleError
     end
   end
+
+class TriangleError < StandardError
+
 end
-
-
-#   class TriangleError < StandardError
-#   end
-# end
