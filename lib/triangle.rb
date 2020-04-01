8736281edeb35aa2b071b9e84e10192bb65bb9c7
Amber Williams <awilliams1234@live.com>
2020-04-01 20:46:10 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,8 @@ class Triangle
   def kind
     if (@first_side <= 0) || (@second_side <= 0) || (@third_side <= 0)
       raise TriangleError
+    elsif (@first_side + @second_side <= @third_side) || (@first_side + @third_side <= @second_side) || (@second_side + @third_side <= @first_side)
+      raise TriangleError
     elsif @first_side > @second_side + @third_side
       raise TriangleError
     elsif @first_side + @second_side < @third_side
@@ -19,10 +21,8 @@ class Triangle
       :equilateral
     elsif @first_side == @second_side && @second_side != @third_side && @side_three != @first_side|| @second_side == @third_side && @third_side != @first_side || @first_side == @third_side && @first_side != @second_side
       :isosceles
-    elsif @first_side != @second_side && @second_side != @third_side
+    else @first_side != @second_side && @second_side != @third_side
       :scalene
-    else (@first_side + @second_side <= @third_side) || (@first_side + @third_side <= @second_side) || (@second_side + @third_side <= @first_side)
-      raise TriangleError
     end
   end
 
