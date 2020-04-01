52f5ea13ed1081d38c7784dabd3dc4698a5fe7b3
Amber Williams <awilliams1234@live.com>
2020-04-01 20:30:30 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,16 +9,18 @@ class Triangle
   def kind
     if (@first_side <= 0) || (@second_side <= 0) || (@third_side <= 0)
       raise TriangleError
+    elsif @first_side + @second_side < @third_side && @first_side + @third_side < @second_side && @first_side > @second_side + @third_side
+      raise TriangleError
     elsif @first_side == @second_side && @second_side == @third_side
       :equilateral
     elsif @first_side == @second_side && @second_side != @third_side && @side_three != @first_side|| @second_side == @third_side && @third_side != @first_side || @first_side == @third_side && @first_side != @second_side
       :isosceles
     elsif @first_side != @second_side && @second_side != @third_side
       :scalene
-    elsif (@first_side + @second_side <= @third_side) || (@first_side + @third_side <= @second_side) || (@second_side + @third_side <= @first_side)
+    else (@first_side + @second_side <= @third_side) || (@first_side + @third_side <= @second_side) || (@second_side + @third_side <= @first_side)
       raise TriangleError
-    else @first_side + @second_side > @third_side && @first_side + @third_side > @second_side && @first_side > @second_side + @third_side
-      raise TriangleError
+    #else @first_side + @second_side > @third_side && @first_side + @third_side > @second_side && @first_side > @second_side + @third_side
+    #  raise TriangleError
     end
   end
 
